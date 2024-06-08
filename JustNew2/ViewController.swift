fc1304fd26a62acc75e1d67217f1bd82ed3755bb
Samandar <samandartoshpulatov0704@gmail.com>
2024-06-08 17:36:21 +0500

    Initial Commit

---
+++ JustNew2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustNew2
+//
+//  Created by Samandar on 08/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
